33776cd952cec1bd355b3795bd575e3212521d44
Halid Ziya Yerebakan <halidziya@gmail.com>
2023-09-27 23:11:55 -0400

    Update speak.js

--- speak.js
+++ speak.js
@@ -1,3 +1,4 @@
+// Function to add the microphone icon to the document
 function addMicrophoneIcon() {
   // Create a container div for the icon and background
   const container = document.createElement("div");
@@ -9,39 +10,40 @@ function addMicrophoneIcon() {
   container.style.borderRadius = "50%"; // Make it round
   container.style.cursor = "pointer"; // Change cursor to pointer
   container.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)"; // Add a shadow
-
+  
   // Create the microphone icon SVG
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("width", "24");
   svg.setAttribute("height", "24");
   svg.innerHTML = '<path d="M12 2c2.76 0 5 2.24 5 5v7c0 2.76-2.24 5-5 5s-5-2.24-5-5v-7c0-2.76 2.24-5 5-5zM19 10v-1a7 7 0 00-14 0v1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>';
-
+  
   // Add a click event listener to start speech recognition
   container.addEventListener("click", startSpeechRecognition);
-
+  
   // Append the SVG to the container
   container.appendChild(svg);
-
+  
   // Append the container to the document body
   document.body.appendChild(container);
 }
 
-
-
+// Function to start speech recognition
 function startSpeechRecognition() {
-  const recognition = const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  // Initialize speech recognition
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
   recognition.lang = "en-US";
-
+  
   recognition.onresult = function (event) {
+    // Get the recognized text
     const result = event.results[0][0].transcript;
     const textarea = document.getElementById("prompt-textarea");
-
-    // Append the recognized text to the textarea
-    textarea.value += result + ' ';
-
+    
+    // Append the recognized text to the textarea followed by a new line
+    textarea.value += result + '\n';
+    
     // Focus on the textarea
     textarea.focus();
-
+    
     // Send a space key press event
     const spaceKeyEvent = new KeyboardEvent('keydown', {
       key: ' ',
@@ -50,25 +52,35 @@ function startSpeechRecognition() {
       which: 32,
       bubbles: true,
     });
-    
     textarea.dispatchEvent(spaceKeyEvent);
 
+    // Send a enter key press event
+    const enterKeyEvent = new KeyboardEvent('keydown', {
+      key: ' ',
+      code: 'Enter',
+      keyCode: 13,
+      which: 13,
+      bubbles: true,
+    });
+    textarea.dispatchEvent(spaceKeyEvent);
+    
+    
     // Trigger an input event to ensure the textarea content is updated
     const inputEvent = new Event('input', {
       bubbles: true,
       cancelable: true,
     });
-
     textarea.dispatchEvent(inputEvent);
   };
-
+  
   recognition.onerror = function (event) {
+    // Handle speech recognition errors
     alert("Speech recognition error: " + event.error);
   };
-
+  
   recognition.start();
-
-  // Listen for changes in the container's children
+  
+  // Observe changes in the container's children
   const container = document.querySelector('.flex.flex-col');
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
@@ -79,12 +91,11 @@ function startSpeechRecognition() {
       }
     });
   });
-
+  
   // Configure the observer to listen for child list changes
   const observerConfig = {
     childList: true,
   };
-
   observer.observe(container, observerConfig);
 }
 
@@ -94,4 +105,5 @@ function speakText(text) {
   window.speechSynthesis.speak(utterance);
 }
 
+// Add the microphone icon when the document is loaded
 addMicrophoneIcon();
